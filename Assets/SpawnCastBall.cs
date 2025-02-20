cff6ecf977f443fffe8ca7a8439616e20a448252
Bill Crosbie <billcrosbie@users.noreply.github.com>
2025-02-19 22:57:56 -0500

    Add mouseClick to initiate cast

--- Assets/SpawnCastBall.cs
+++ Assets/SpawnCastBall.cs
@@ -9,6 +9,17 @@ public class SpawnCastBall : MonoBehaviour
     public GameObject spawnPoint;
     public float speed = 30f;
 
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator not set!!!");
+        }
+    }
+
     public void CastSpell()
     {
         GameObject go = Instantiate(
@@ -18,4 +29,12 @@ public class SpawnCastBall : MonoBehaviour
         go.GetComponent<Rigidbody>().velocity = new Vector3(speed,0,0);
     }
 
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("castTrigger");
+        }
+    }
 }
